15dc77c474858d34579ef06ad2321762ac25e1ad
asteriskzuo <zuoyuhsywn@hotmail.com>
2025-07-07 14:01:47 +0800

    chore: update uikit

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -5,136 +6,107 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import * as React from 'react';
+import {Pressable, SafeAreaView, Text, View} from 'react-native';
 import {
-  Button,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  Container,
+  ConversationDetail,
+  TextInput,
+  useChatContext,
+} from 'react-native-chat-uikit';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import {multiply} from 'react-native-chat-uikit';
+const appKey = 'easemob#easeim';
+const userId = 'du004';
+const userPassword = '1';
+const usePassword = true; // or false;
+const peerId = 'du005';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function SendMessage() {
+  const [page, setPage] = React.useState(0);
+  const [_appKey, setAppKey] = React.useState(appKey);
+  const [id, setId] = React.useState(userId);
+  const [ps, setPs] = React.useState(userPassword);
+  const [peer, setPeer] = React.useState(peerId);
+  const im = useChatContext();
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  // return (
+  //   <View style={{flex: 1, backgroundColor: 'red'}}></View>
+  // );
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
-
-  return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header />
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-        <Button
-          title="Click me"
-          onPress={() => {
-            console.log('click');
-            console.log(multiply(1, 2));
-          }}
+  if (page === 0) {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <TextInput
+          placeholder="Please App Key."
+          value={_appKey}
+          onChangeText={setAppKey}
         />
-        <View style={{height: 100, width: 100, backgroundColor: 'red'}} />
-      </ScrollView>
-    </View>
-  );
+        <TextInput
+          placeholder="Please Login ID."
+          value={id}
+          onChangeText={setId}
+        />
+        <TextInput
+          placeholder="Please Login token or password."
+          value={ps}
+          onChangeText={setPs}
+        />
+        <TextInput
+          placeholder="Please peer ID."
+          value={peer}
+          onChangeText={setPeer}
+        />
+        <Pressable
+          onPress={() => {
+            im.login({
+              userId: id,
+              userToken: ps,
+              usePassword: usePassword,
+              result: res => {
+                console.log('login result', res);
+                if (res.isOk === true) {
+                  setPage(1);
+                }
+              },
+            });
+          }}>
+          <Text>{'Login'}</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            im.logout({
+              result: () => {},
+            });
+          }}>
+          <Text>{'Logout'}</Text>
+        </Pressable>
+      </SafeAreaView>
+    );
+  } else if (page === 1) {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <ConversationDetail
+          convId={peer}
+          convType={0}
+          onBack={() => {
+            setPage(0);
+            im.logout({
+              result: () => {},
+            });
+          }}
+          type={'chat'}
+        />
+      </SafeAreaView>
+    );
+  } else {
+    return <View />;
+  }
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default function App(): React.JSX.Element {
+  return (
+    <Container options={{appKey: appKey, autoLogin: false}}>
+      <SendMessage />
+    </Container>
+  );
+}
